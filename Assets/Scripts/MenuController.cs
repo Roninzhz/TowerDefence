3e90cc5ea71a6d4b1f794f711d04a5f4fe049ab5
1938917626@qq.com <1938917626@qq.com>
2021-06-30 19:24:23 +0800

    1.0版本 更新：添加开始场景，未实现跳转

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	//AboutImage组件
+	public GameObject AboutImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//打开关于作者
+	public void AboutBtnClick()
+    {
+		AboutImage.SetActive(true);
+    }
+
+	//退出游戏
+	public void ExitGameBtnClick()
+    {
+		UnityEditor.EditorApplication.isPlaying = false;
+		Application.Quit();
+    }
+}
